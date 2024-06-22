e0b74494cf2900e51e1dd33ad97f1b99914a0a99
Anum Javed <anum.j147@gmail.com>
2024-06-22 16:59:48 +0500

    Create Model for the contact form and register the model in admin file. Update the view function and save data.

--- portfolio/base/views.py
+++ portfolio/base/views.py
@@ -1,9 +1,22 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import ContactMessage
 # Create your views here.
 
 def home(request):
-  return render(request, 'web.html')
+  if request.method == 'POST':
+    # Process form submission
+    first_name = request.POST.get('first_name')
+    last_name = request.POST.get('last_name')
+    email = request.POST.get('email')
+    subject = request.POST.get('subject')
+    message = request.POST.get('message')
+    
+    entry = ContactMessage(first_name=first_name, last_name=last_name, email=email, subject=subject, message=message)
+    entry.save()
+    
+    return redirect('home')
+  else:
+    return render(request, 'web.html')
 
 def project1(request):
   return render(request, 'project1.html')
